efc16bb4328dac75be1cdc74b6e38422650564bb
Vincent Bergeron <vincentbergeron33@gmail.com>
2023-03-08 06:59:52 +0000

    Fix movetwo, Fix isWithinBoard, Fix Rook move and capture position

--- src/Piece.py
+++ src/Piece.py
@@ -33,54 +33,63 @@ class Piece:
             return self.rookValidMoveAndCapturePositions(board)
 
         else:
-            raise NotImplemented
+            raise NotImplementedError()
 
 
 # Vince adding code here
 
-    def isWithinBoard(location) -> Bool:
-        if 0 <= location[0] >= 7 and  0 <= location[1] >= 7 return True
-
-
-    def pawnCanMoveTwo(location, ColorPiece):
-        if ColorPiece is WHITE:
-            location_to_validate_ = [
-            board.pieces[location[0], location[1] + 1],
-            board.pieces[location[0], location[1] + 2]
-            ]
+    def isWithinBoard(location) -> bool:
+        if 0 <= location[0] >= 7 and 0 <= location[1] >= 7:
+            return True
         else:
-            location_to_validate_ = [
-            board.pieces[location[0], location[1] - 1],
-            board.pieces[location[0], location[1] - 2]
-            ]
-               
-    piece_pawn_can_move_two : List [Piece] = (lambda piece: self.location is not piece.location, location_to_validate)
-    return piece_pawn_can_move_two
+            return False
+
+
+    def pawnCanMoveTwo(self, location, ColorPiece, board, piece):
+        if self.ColorPiece is ColorPiece.WHITE:
+            if board.pieces[location[0], location[1] + 1] is not piece.location:
+                location_to_validate = [
+                    board.pieces[location[0], location[1] + 1],
+                    board.pieces[location[0], location[1] + 2]
+                ]
+            else:
+                return None
+        else:
+            if board.pieces[location[0], location[1] - 1] is not piece.location:
+                location_to_validate = [
+                    board.pieces[location[0], location[1] - 1],
+                    board.pieces[location[0], location[1] - 2]
+                ]
+
+            else:
+                return None
+
+        piece_pawn_can_move_two: List[Piece] = (lambda piece: self.location is not piece.location, location_to_validate)
+        return piece_pawn_can_move_two
 
     def pawnValidMovePositions(self, board: Board) -> List[(int, int)]:
-        if self.colorPiece is ColorPiece.WHITE:
+        
+        if self.location is self.startingLocation:
+            position_pawn_can_move = pawnCanMoveTwo(self.location, self.colorPiece)
+        elif self.colorPiece is ColorPiece.WHITE:
             location_to_validate: List[Piece] = [
             board.pieces[self.location[0]][self.location[1] + 1]
             ]
+            piece_pawn_can_move: List [Piece] = (lambda piece: self.location is not piece.location and isWithinBoard(), location_to_validate)
         else:
             location_to_validate: List[Piece] = [
             board.pieces[self.location[0]][self.location[1] - 1]
             ]
-        
-        piece_pawn_can_move : List [Piece] = (lambda piece: self.location is not piece.location, location_to_validate)
+            piece_pawn_can_move: List [Piece] = (lambda piece: self.location is not piece.location and isWithinBoard(), location_to_validate)
 
-        if self.location is self.startingLocation:
-            pawn_can_move_two = pawnCanMoveTwo(self.location, self.colorPiece)
-            position_pawn_can_move = position_pawn_can_move.append(pawn_can_move_two)
-        
         return position_pawn_can_move
+             
         
 # End of Vince's code for pawn movement. I guess the raise NotImplemented is not required? Now that we have the available
 # position to move and capture, we can call the functions in the PawnValidMovePositions? We are just missing the last restriction
 # which is the board
 
-
-    def pawnValidCapturePositions(self, board:Board) -> List[(int, int)]:
+    def pawnValidCapturePositions(self, board: Board) -> List[(int, int)]:
         if self.colorPiece is ColorPiece.WHITE:
             pieces_to_validate: List[Piece] = [
                 board.pieces[self.location[0] + 1][self.location[1] + 1],
@@ -98,7 +107,7 @@ class Piece:
         return positions_pawn_can_capture
 
 #Vince adding ROOK here
-    def rookValidMoveAndCapturePositions(self, board: Board) -> List[(int, int)]:
+    def rookValidMoveAndCapturePositions(self, board: Board, piece) -> List[(int, int)]:
         piece_to_validate_up = [[board.pieces[self.location[0]][self.location[1] + 1]]]
         valide_move_and_capture_location_rook = []
 
@@ -110,10 +119,10 @@ class Piece:
             break
 
         else:
-            while piece_to_validate_up is not piece.location and withinBoard(self.location):
-                location_up_rook: Liste[Piece] = []
+            while piece_to_validate_up is not piece.location and isWithinBoard(piece_to_validate_up):
+                location_up_rook: List[Piece] = []
                 location_up_rook.append(piece_to_validate_up)
-                piece_to_validate_up = [piece_to_validate_up[0]][piece_to_validate_up[1] + 1]
+                piece_to_validate_up = board.pieces[[piece_to_validate_up[0]][piece_to_validate_up[1] + 1]]
             return location_up_rook
 
 
@@ -130,7 +139,7 @@ class Piece:
             while piece_to_validate_down is not piece.location and isWithinBoard(piece_to_validate_down):
                 location_down_rook: Liste[Piece] = []
                 location_down_rook.append(piece_to_validate_down)
-                piece_to_validate_down = [piece_to_validate_down[0]][piece_to_validate_down[1] - 1]
+                piece_to_validate_down = board.pieces[[piece_to_validate_down[0]][piece_to_validate_down[1] - 1]]
             return location_down_rook
 
 
@@ -144,10 +153,10 @@ class Piece:
             break
 
         else:
-            while piece_to_validate_right is not piece.location and withinBoard(self.location):
+            while piece_to_validate_right is not piece.location and isWithinBoard(piece_to_validate_right):
                 location_right_rook: Liste[Piece] = []
                 location_right_rook.append(piece_to_validate_right)
-                piece_to_validate_right = [piece_to_validate_right[0] + 1][piece_to_validate_right[1]]
+                piece_to_validate_right = board.pieces[[piece_to_validate_right[0] + 1][piece_to_validate_right[1]]]
             return location_right_rook
 
 
@@ -161,10 +170,10 @@ class Piece:
             break
 
         else:
-            while piece_to_validate_left is not piece.location and withinBoard(self.location):
+            while piece_to_validate_left is not piece.location and isWithinBoard(piece_to_validate_left):
                 location_left_rook: Liste[Piece] = []
                 location_left_rook.append(piece_to_validate_left)
-                piece_to_validate_left = [piece_to_validate_left[0] - 1][piece_to_validate_left[1]]
+                piece_to_validate_left = board.pieces[[piece_to_validate_left[0] - 1][piece_to_validate_left[1]]]
             return location_left_rook
 
     valide_move_and_capture_location_rook = valide_move_and_capture_location_rook.append(location_up_rook)
@@ -172,8 +181,9 @@ class Piece:
     valide_move_and_capture_location_rook = valide_move_and_capture_location_rook.append(location_right_rook)
     valide_move_and_capture_location_rook = valide_move_and_capture_location_rook.append(location_left_rook)
 
-    valide_move_and_capture_location_rook = location_up_rook + location_down_rook + location_right_rook + location_left_rook 
+    #valid position to capture to be added
 
     return valide_move_and_capture_location_rook
 
 
+    #Vince adding BISHOP here
